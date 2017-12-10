875e3717dd7a4bf6a53bc43771a4c989754d0fe9
shtan <6224018+shtan@users.noreply.github.com>
2017-12-10 11:08:34 +0100

    Added stochastic envelope timbre scaling option

--- software/transformations/stochasticTransformations.py
+++ software/transformations/stochasticTransformations.py
@@ -2,24 +2,56 @@
 
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy.signal import hanning, resample
 
 
 def stochasticTimeScale(stocEnv, timeScaling):
-	"""
-	Time scaling of the stochastic representation of a sound
-	stocEnv: stochastic envelope
-	timeScaling: scaling factors, in time-value pairs
-	returns ystocEnv: stochastic envelope
-	"""
-	if (timeScaling.size % 2 != 0):                             # raise exception if array not even length
-		raise ValueError("Time scaling array does not have an even size")
-		
-	L = stocEnv[:,0].size                                       # number of input frames
-	outL = int(L*timeScaling[-1]/timeScaling[-2])               # number of synthesis frames
-	# create interpolation object with the time scaling values
-	timeScalingEnv = interp1d(timeScaling[::2]/timeScaling[-2], timeScaling[1::2]/timeScaling[-1])
-	indexes = (L-1)*timeScalingEnv(np.arange(outL)/float(outL)) # generate output time indexes
-	ystocEnv = stocEnv[0,:]                                     # first output frame is same than input
-	for l in indexes[1:]:                                       # step through the output frames
-		ystocEnv = np.vstack((ystocEnv, stocEnv[int(round(l)),:]))     # get the closest input frame
-	return ystocEnv
+    """
+    Time scaling of the stochastic representation of a sound
+    stocEnv: stochastic envelope
+    timeScaling: scaling factors, in time-value pairs
+    returns ystocEnv: stochastic envelope
+    """
+    if (timeScaling.size % 2 != 0):                             # raise exception if array not even length
+        raise ValueError("Time scaling array does not have an even size")
+        
+    L = stocEnv[:,0].size                                       # number of input frames
+    outL = int(L*timeScaling[-1]/timeScaling[-2])               # number of synthesis frames
+    # create interpolation object with the time scaling values
+    timeScalingEnv = interp1d(timeScaling[::2]/timeScaling[-2], timeScaling[1::2]/timeScaling[-1])
+    indexes = (L-1)*timeScalingEnv(np.arange(outL)/float(outL)) # generate output time indexes
+    ystocEnv = stocEnv[0,:]                                     # first output frame is same than input
+    for l in indexes[1:]:                                       # step through the output frames
+        ystocEnv = np.vstack((ystocEnv, stocEnv[int(round(l)),:]))     # get the closest input frame
+    return ystocEnv
+
+def stochasticTimbreScale(stocEnv_nodecimate, timbreScalingStoc, maxFreqFrac, stocf):
+    """
+    Stochastic analysis of a sound
+    x: input array sound, H: hop size, N: fftsize
+    stocf: decimation factor of mag spectrum for stochastic analysis, bigger than 0, maximum of 1
+    returns stocEnv: stochastic envelope
+    """
+    
+    L = stocEnv_nodecimate.shape[0]                           # Number of time frames
+    #nModes = stocEnv_nodecimate.shape[1]                      # Number of DFT modes
+    stocEnv_shifted = []
+    
+    for l in range(0,L):
+        mX = stocEnv_nodecimate[l,:]
+        nModes = mX.size                                      # Number of DFT modes
+        mXshift = np.zeros(nModes)
+        
+        for i in range(0, nModes):
+            if (round(float(i)/timbreScalingStoc)<nModes and (i/nModes)<maxFreqFrac):
+                mXshift[i] = mX[int(round(float(i)/timbreScalingStoc))]
+            else:
+                mXshift[i] = mX[i]
+        mY = resample(np.maximum(-200, mXshift), int(stocf*nModes))  # decimate the mag spectrum 
+        stocEnv_shifted.append(np.array(mY))
+        
+    stocEnv_shifted = np.array(stocEnv_shifted)
+
+    return stocEnv_shifted
+
+
